d188433adbf686f57e2f9cee51f09f941431faac
Alan <alance10a@gmail.com>
2022-05-24 15:02:20 -0300

    Adivino.py agregado

---
+++ adivino.py
@@ -0,0 +1,26 @@
+import random
+
+print("Bienvenido.")
+print("Comienzo del programa.")
+print()
+
+numero_aleatorio = random.randrange(101)
+gane = False
+
+print("Tenés 5 intentos para adivinar un entre 0 y 100.")
+print()
+intento = 1
+
+while intento < 6 and not gane:
+    numero_ingresado = int(input('Ingresa tu número: '))   
+    if numero_ingresado == numero_aleatorio:
+        print('Ganaste! y necesitaste {} intentos!!!'.format(intento))
+        gane = True    
+    else:
+        print('Mmmm ... No.. ese número no es... Seguí intentando.')
+        intento += 1
+    print()
+if not gane:
+    print('\n Perdiste :(\n El número era: {}'.format(numero_aleatorio))
+
+print()
